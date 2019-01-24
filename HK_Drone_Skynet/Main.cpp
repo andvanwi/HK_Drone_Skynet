790fe369a53702a6005dc54aba9cf372fd718b4c
ANDREW VAN WINKLE <ANDVANWI@uat.edu>
2019-01-24 12:41:27 -0700

    Add project files.

---
+++ HK_Drone_Skynet/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// code block where we place our code
+	
+	// welcome user
+	cout << "Check!" << endl;
+
+	// pause screen for user
+	system("pause");
+	return;
+}
